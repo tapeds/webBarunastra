bdc7d5a7c00fd0a70451717bf22bfa79c55063af
tapeds <frrllmatthew@gmail.com>
2023-01-08 20:08:49 +0700

    fixing not showing images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,4 @@
 import * as React from 'react';
-import Link from 'next/link';
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
-import Image from 'next/image'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Start from '@/components/Start';
@@ -18,7 +10,6 @@ import Divisionpage from '@/components/Divisionpage';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 import Page2 from '@/components/Page2';
 import Achievementpage from '@/components/Achievementpage';
 import Quote from '@/components/Quote';
